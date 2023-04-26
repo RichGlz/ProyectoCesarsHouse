61e536227c2980426d9437697249064db1162c6c
Rich González <ragparkour@gmail.com>
2023-04-25 19:29:29 -0600

    Se agregan notas al archivo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <Arduino.h>
 
 
+/*
+    Ventilador de cocina con temperatura
+    Variables: Sensor de temperatura, Relay, sensor de apertura
+    FORMULA: Si sensor de temperatura > 33ºC && puerta cerrada, entonces encender el relay
+
+    Luz de emergencia
+    Variables: lector de corriente, sí hay corriente entonces 
+    Activación por electrónica: Relay NO=corriente electrica , OUT= tira LED
+
+
+    Relojes que funcionan con batería
+
+    PENDIENTE
+    
+
+*/
 
 
 void setup() {
